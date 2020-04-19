3595426baec41c30c798ec7e144530ecf60e1386
Alvaro Pozo <alvaro.pozo.torres@gmail.com>
2020-04-19 18:21:23 -0400

    final de seccion 5
    Ultimas lecciones. interfaces

--- classesAndInterfaces/src/app.ts
+++ classesAndInterfaces/src/app.ts
@@ -1,121 +1,51 @@
-abstract class Department {
-     static fiscalYear = 2020;
-     
-     protected employees: string[] = [];
-
-     constructor (protected readonly id: string, public name: string) {
-        // this.name = n;
-     }
-
-     static createEmployee(name: string) {
-         return {name: name};
-     }
-
-     abstract describe(this: Department): void;
-
-     addEmployee(employee:string) {
-         this.employees.push(employee);
-     }
-
-     printEmployeesInformation() {
-        console.log('total employees: ' + this.employees.length);
-        console.log('employees names: ' + this.employees);
-     }
+//START. using interface as a custum type
+interface AddFn {
+    (a:number, b:number): number;
 }
 
-class ITDepartment extends Department {
-    admins: string[];
+let add: AddFn;
 
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT');
-        this.admins = admins;
-    }
-
-    describe() {
-        console.log('IT department ID: ' + this.id);
-    }
+add = (n1:number, n2: number) => {
+    return n1 + n2;
 }
 
-class AccountingDepartment extends Department {
-    private lastReport: string;
-    private static instance: AccountingDepartment;
+console.log(add(5, 4));
+//END. using interface as a custum type
 
 
-
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return 'Last report: ' + this.lastReport;
-        }
-        throw new Error('No last report found');        
-    }
-
-    set mostRecentReport(value: string) {
-        this.addReport(value);
-    }
-
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting');
-        this.lastReport = reports[0];
-    }
-
-    static getInstance() {
-        if (this.instance) {
-            return (this.instance);
-        }
-        this.instance = new AccountingDepartment('Acc', [])
-        return this.instance;
-    }
-
-    describe() {
-        console.log('Accounting department ID: ' + this.id);
-    }
-
-    addEmployee(name: string) {
-        if (name === 'Max') {
-            console.log(`No se puede registrar a ${name} dentro del departamento ${this.name}`);
-            return;
-        }
-        this.employees.push(name);
-    }
-    
-    addReport(text: string) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-
-    printReports() {
-        console.log('Reports: ' + this.reports);
-    }
+interface Named {
+    readonly name?: string;
+    outputName?: string;        //  el simbolo ? hace que la implementacion del elemento sea opcional en la clase
 }
 
-const employee1 = Department.createEmployee('NewEmployee');
-console.log(employee1, Department.fiscalYear);
+interface Greetable extends Named{
+    greet(phrase: string): void;
+}
 
-// const accounting = new AccountingDepartment('acc01', []);
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-console.log(accounting);
-accounting.addEmployee('Alvaro');
-accounting.addEmployee('Max');
+class Person implements Greetable {
+    name?: string;
+    age = 42;
 
-console.log(accounting, accounting2);
+    constructor(n?:string) {
+        if (n) this.name = n;
+    }
 
-accounting.describe();
-accounting.printEmployeesInformation();
-accounting.addReport('report 01');
-accounting.addReport('report 02');
-accounting.printReports();
-console.log(accounting.mostRecentReport);
-accounting.mostRecentReport = 'report 03';
-accounting.printReports();
-console.log(accounting.mostRecentReport);
+    greet(phrase: string) {
+        if (this.name) {
+            console.log('Hello '+ this.name + '. ' + phrase);
+        } else {
+            console.log('Hello there');
+        }
+    }
 
-console.log(accounting, accounting2);
+}
+let user1: Greetable;
 
-const itDep = new ITDepartment('it01', ['pau', 'naya']);
-console.log(itDep);
-itDep.addEmployee('Leo');
-itDep.addEmployee('Trainer');
+user1 = new Person ()
+user1.greet('Como va todo?');
+console.log(user1);
 
-itDep.describe();
-itDep.printEmployeesInformation();
+let user2: Greetable;
+user2 = new Person('Bruno');
+user2.greet('Como va todo?');
+console.log(user2);
